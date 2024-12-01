c3cd90af2f4650516eabcfb9904a183ba9a2036e
Isma <shadqi1890@gmail.com>
2024-12-01 16:15:23 +0700

    PBL

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\pelangganController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('pelanggan/create', [pelangganController::class, 'create'])->name('pelanggan.create');
+Route::post('pelanggan', [pelangganController::class, 'store'])->name('pelanggan.store');
+Route::resource('/pelanggan', pelangganController::class);
+
+
